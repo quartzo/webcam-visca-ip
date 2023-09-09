bd838d7871e962498aa2fd5baf7bcb932abc8242
Fabio Ferrari <fabio@ferraribr.com>
2023-09-09 12:53:43 -0300

    color matrix solved

--- src/teleport.rs
+++ src/teleport.rs
@@ -88,29 +88,98 @@ static HOSTNAME: Lazy<String> = Lazy::new(||
     hostname::get().expect("No Hostname?").into_string().expect("Bad string from Hostame")
 );
 
-static COLOR_MATRIX: Lazy<Vec<u8>> = Lazy::new(|| {
+
+static COLOR_RANGE_MIN: [f32; 3] = [0.,0.,0.];
+static COLOR_RANGE_MAX: [f32; 3] = [1.,1.,1.];
+static COLOR_RANGE_MIN_BYTES: Lazy<Vec<u8>> = Lazy::new(|| {
     let mut r = Vec::new();
-    for i in 0..16 {
-        if i % 5 == 0 {
-            r.extend_from_slice(&(1.0 as f32).to_le_bytes());
-        } else {
-            r.extend_from_slice(&(0.0 as f32).to_le_bytes());
+    for i in 0..3 {
+        r.extend_from_slice(&COLOR_RANGE_MIN[i].to_le_bytes());
+    }
+    r
+});
+static COLOR_RANGE_MAX_BYTES: Lazy<Vec<u8>> = Lazy::new(|| {
+    let mut r = Vec::new();
+    for i in 0..3 {
+        r.extend_from_slice(&COLOR_RANGE_MAX[i].to_le_bytes());
+    }
+    r
+});
+static COLOR_MATRIX_BYTES: Lazy<Vec<u8>> = Lazy::new(|| {
+    //VIDEO_CS_709
+    let kb: f32 = 0.0722;
+    let kr: f32 = 0.2126;
+    let kg = 1. - kb - kr;
+
+    struct Vec3 {
+        x: f32, y: f32, z: f32
+    }
+    struct Matrix3 {
+        x: Vec3, y: Vec3, z: Vec3
+    }
+    fn vec3_dot(v1: &Vec3, v2: &Vec3) -> f32 {
+        v1.x*v2.x + v1.y*v2.y + v1.z*v2.z
+    }
+    fn vec3_rotate(v: &Vec3, m: &Matrix3) -> Vec3 {
+        Vec3{
+            x: vec3_dot(&v, &m.x),
+            y: vec3_dot(&v, &m.y),
+            z: vec3_dot(&v, &m.z)
         }
     }
-    r
-});
-static COLOR_RANGE_MIN: Lazy<Vec<u8>> = Lazy::new(|| {
+
+    let min_value:f32 = 16.;
+    let max_luma:f32 = 235.;
+	let max_chroma:f32 = 240.;
+    let mid_chroma:f32 = 0.5 * (min_value + max_chroma);
+    let range_min: [f32; 3] = [min_value,min_value,min_value];
+    let range_max: [f32; 3] = [max_luma,max_chroma,max_chroma];
+    let black_levels: [f32; 3] = [0., mid_chroma, mid_chroma];
+
+	let yvals: f32 = range_max[0] - range_min[0];
+	let uvals: f32 = (range_max[1] - range_min[1]) / 2.;
+	let vvals: f32 = (range_max[2] - range_min[2]) / 2.;
+
+    let bit_range_max: f32 = 256. - 1.;
+	let yscale: f32 = bit_range_max / yvals;
+	let uscale: f32 = bit_range_max / uvals;
+	let vscale: f32 = bit_range_max / vvals;
+
+    let color_matrix: Matrix3 = Matrix3 {
+        x: Vec3{x:yscale, y:0., z:vscale * (1. - kr)},
+        y: Vec3{x:yscale, y:uscale * (kb - 1.) * kb / kg, z:vscale * (kr - 1.) * kr / kg},
+        z: Vec3{x:yscale, y:uscale * (1. - kb), z:0.}
+    };
+
+	let offsets: Vec3 = Vec3{x:-black_levels[0] / bit_range_max,
+		 y:-black_levels[1] / bit_range_max,
+		 z:-black_levels[2] / bit_range_max};
+    let multiplied: Vec3 = vec3_rotate(&offsets, &color_matrix);
+
+    let mut matrix:[f32; 16] = [0.; 16];
+    matrix[0] = color_matrix.x.x;
+	matrix[1] = color_matrix.x.y;
+	matrix[2] = color_matrix.x.z;
+	matrix[3] = multiplied.x;
+
+	matrix[4] = color_matrix.y.x;
+	matrix[5] = color_matrix.y.y;
+	matrix[6] = color_matrix.y.z;
+	matrix[7] = multiplied.y;
+
+	matrix[8] = color_matrix.z.x;
+	matrix[9] = color_matrix.z.y;
+	matrix[10] = color_matrix.z.z;
+	matrix[11] = multiplied.z;
+
+	matrix[12] = 0.; matrix[13] = 0.; matrix[14] = 0.;
+	matrix[15] = 1.;
+
     let mut r = Vec::new();
-    for _ in 0..3 {
-        r.extend_from_slice(&(0.0 as f32).to_le_bytes());
+    for i in 0..16 {
+        r.extend_from_slice(&matrix[i].to_le_bytes());
     }
-    r
-});
-static COLOR_RANGE_MAX: Lazy<Vec<u8>> = Lazy::new(|| {
-    let mut r = Vec::new();
-    for _ in 0..3 {
-        r.extend_from_slice(&(1.0 as f32).to_le_bytes());
-    }
+    //println!("color matrix {:?}", matrix);
     r
 });
 
@@ -273,9 +342,9 @@ impl TeleportCam {
             r.extend_from_slice(&timestamp.to_le_bytes());
             r.extend_from_slice(&size.to_le_bytes());
             // ImageHeader
-            r.extend_from_slice(&COLOR_MATRIX);
-            r.extend_from_slice(&COLOR_RANGE_MIN);
-            r.extend_from_slice(&COLOR_RANGE_MAX);
+            r.extend_from_slice(&COLOR_MATRIX_BYTES);
+            r.extend_from_slice(&COLOR_RANGE_MIN_BYTES);
+            r.extend_from_slice(&COLOR_RANGE_MAX_BYTES);
             // data
             let sz = get_jpeg_size(buf)?;
             //println!("sz {:?}", sz);
